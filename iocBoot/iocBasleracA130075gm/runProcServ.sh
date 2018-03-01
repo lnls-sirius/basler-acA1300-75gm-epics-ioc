f3ae35c06cab63d714f088f03e7230ab2aa2db99
andrei_pereira <andrei.pereira@lnls.br>
2018-03-01 08:51:06 -0300

    iocBoot/ioc*/runProcServ.sh: Update camera env variables' names
    The env variables needed to be updated in order to agree with systemd script settings.

--- iocBoot/iocBasleracA130075gm/runProcServ.sh
+++ iocBoot/iocBasleracA130075gm/runProcServ.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Use defaults if not set
-if [ -z "${BASLER_ACA130075GM_DEVICE_TELNET_PORT}" ]; then
-   BASLER_ACA130075GM_DEVICE_TELNET_PORT=20000
+if [ -z "${BASLER_DEVICE_TELNET_PORT}" ]; then
+   BASLER_DEVICE_TELNET_PORT=20000
 fi
 
 # Run run*.sh scripts with procServ
-/usr/local/bin/procServ -f -n basler_acA1300_75gm_${BASLER_ACA130075GM_INSTANCE} -i ^C^D ${BASLER_ACA130075GM_DEVICE_TELNET_PORT} ./runBasleracA130075gm.sh "$@"
+/usr/local/bin/procServ -f -n basler_acA1300_75gm_${BASLER_INSTANCE} -i ^C^D ${BASLER_DEVICE_TELNET_PORT} ./runBasleracA130075gm.sh "$@"

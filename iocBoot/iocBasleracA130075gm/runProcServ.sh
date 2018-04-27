f37e3de650935ca34f7a7f9b17d7cc81662af7fc
andrei_pereira <andrei.pereira@lnls.br>
2018-04-27 09:30:45 -0300

    iocBoot/iocBasleracA130075gm/{parseCMDOpts.sh. run*}: Add option for procServ telnet port

--- iocBoot/iocBasleracA130075gm/runProcServ.sh
+++ iocBoot/iocBasleracA130075gm/runProcServ.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
+set -e
+set +u
+
+# Parse command-line options
+. ./parseCMDOpts.sh "$@"
+
 # Use defaults if not set
-if [ -z "${BASLER_DEVICE_TELNET_PORT}" ]; then
-   BASLER_DEVICE_TELNET_PORT=20000
+if [ -z "${DEVICE_TELNET_PORT}" ]; then
+   DEVICE_TELNET_PORT="20000"
 fi
 
+if [ -z "${BASLER_INSTANCE}" ]; then
+   BASLER_INSTANCE="1"
+fi
+
+set -u
+
 # Run run*.sh scripts with procServ
-/usr/local/bin/procServ -f -n basler_acA1300_75gm_${BASLER_INSTANCE} -i ^C^D ${BASLER_DEVICE_TELNET_PORT} ./runBasleracA130075gm.sh "$@"
+/usr/local/bin/procServ -f -n basler_acA1300_75gm_${BASLER_INSTANCE} -i ^C^D ${DEVICE_TELNET_PORT} ./runBasleracA130075gm.sh "$@"

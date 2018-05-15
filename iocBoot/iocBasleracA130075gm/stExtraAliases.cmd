3430e84958d68f863bb74141036a22f38f20440a
andrei_pereira <andrei.pereira@lnls.br>
2018-05-15 15:57:20 -0300

    iocBoot/iocBasleracA130075gm/st*, configure/RELEASE.local.example: Adapt files to use areaDetector-R3-2

--- iocBoot/iocBasleracA130075gm/stExtraAliases.cmd
+++ iocBoot/iocBasleracA130075gm/stExtraAliases.cmd
@@ -34,12 +34,12 @@ dbLoadRecords("$(ARAVISGIGE)/db/aravisCamera.template", "P=$(PREFIX),R=cam1:,POR
 dbLoadRecords("$(TOP)/db/Basler_acA1300_75gm.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
 # Create a standard arrays plugin
-NDStdArraysConfigure("Image1", 5, 0, "$(PORT)", 0, 0)
+NDStdArraysConfigure("Image1", "$(QSIZE)", 0, "$(PORT)", 0, 0, 0, 0)
 # Allow for cameras up to 1280x1024x3 for RGB
 dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int16,FTVL=SHORT,NELEMENTS=3932160")
 
 # Load all other plugins using commonPlugins.cmd
-< $(ADCORE)/iocBoot/commonPlugins.cmd
+< $(ADCORE)/iocBoot/EXAMPLE_commonPlugins.cmd
 
 # Sirius-naming-convention compliant PVs
 dbLoadRecords("$(TOP)/db/BaslerSiriusStandard.db","P=$(P),R=$(R)")

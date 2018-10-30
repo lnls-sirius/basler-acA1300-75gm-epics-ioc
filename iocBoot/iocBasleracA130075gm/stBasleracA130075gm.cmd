657bf89bbe9a3b5d298618e28808975a1a8b6bbd
andrei_pereira <andrei.pereira@lnls.br>
2018-10-30 12:44:20 -0300

    Fix problem of Data-Mon PV .NORD field not changing

--- iocBoot/iocBasleracA130075gm/stBasleracA130075gm.cmd
+++ iocBoot/iocBasleracA130075gm/stBasleracA130075gm.cmd
@@ -39,7 +39,7 @@ dbLoadRecords("$(TOP)/db/Basler_acA1300_75gm.template","P=$(PREFIX),R=Cam1,PORT=
 # Create a standard arrays plugin
 NDStdArraysConfigure("Image1", "$(QSIZE)", 0, "TRANSF1", 0, 0, 0, 0)
 # Allow for cameras up to 1280x1024x3 for RGB
-dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=Image1,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TRANSF1,TYPE=Int16,FTVL=SHORT,NELEMENTS=3932160")
+dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=Image1,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=TRANSF1,TYPE=Int16,FTVL=USHORT,NELEMENTS=3932160")
 
 # Sirius-naming-convention compliant PVs
 dbLoadRecords("$(TOP)/db/BaslerSiriusStandard.db","P=$(P),R=$(R)")
